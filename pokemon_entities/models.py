a671082add2bd526b3a1e6ed624025b7e58f666a
gardnervile <lordy1jo@gmail.com>
2025-01-15 22:50:14 +0500

    create Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,10 @@
 from django.db import models  # noqa F401
 
+class Pokemon(models.Model):
+    title = models.CharField(max_length=255)
+
+def __str__(self):
+    return self.title
+
+
 # your models here
