c7f845ed0aa98cabd370ce4c85723ac86d82084b
gardnervile <lordy1jo@gmail.com>
2025-01-16 23:03:09 +0500

    show pokemons on mainpage map.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -3,7 +3,8 @@ import json
 
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from .models import Pokemon
+from .models import Pokemon, PokemonEntity
+
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -27,23 +28,36 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
-    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
-    for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
     pokemons = Pokemon.objects.all()
+    pokemon_entities = PokemonEntity.objects.all()
+    folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
+
+    for pokemon_entity in pokemon_entities:
+        image_url = None
+        if pokemon_entity.pokemon.image:
+            image_url = request.build_absolute_uri(pokemon_entity.pokemon.image.url)
+        
+        if image_url:
+            add_pokemon(
+                folium_map,
+                pokemon_entity.lat,
+                pokemon_entity.lon,
+                image_url
+            )
+        else:
+            add_pokemon(
+                folium_map,
+                pokemon_entity.lat,
+                pokemon_entity.lon,
+                request.build_absolute_uri('/static/default_icon.png')
+            )
+
     pokemons_on_page = []
     for pokemon in pokemons:
         img_url = None
         if pokemon.image:
             img_url = request.build_absolute_uri(pokemon.image.url)
+
         pokemons_on_page.append({
             'pokemon_id': pokemon.id,
             'img_url': img_url,
