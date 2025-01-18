be04d8efe4349931a37b342e58ec67f6fc5c53fe
gardnervile <lordy1jo@gmail.com>
2025-01-18 23:36:39 +0500

    admin in russian

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,14 +1,14 @@
 from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='pokemons/', null=True, blank=True)
-    description = models.TextField(blank=True)
-    title_en  = models.CharField(max_length=255, null=True)
-    title_jp = models.CharField(max_length=255, null=True)
-    evolved_from = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='evolutions')
-    evolves_to = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='evolves_from')
-    
+    title = models.CharField(max_length=255, verbose_name='Имя покемона')
+    image = models.ImageField(upload_to='pokemons/', null=True, blank=True,verbose_name='Изображение покемона')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    title_en  = models.CharField(max_length=255, null=True, verbose_name='Имя покемона на английском')
+    title_jp = models.CharField(max_length=255, null=True, verbose_name='Имя покемона на японском')
+    evolved_from = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='evolutions', verbose_name='Эволюционирует из')
+    evolves_to = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL, related_name='evolves_from', verbose_name='Эволюционирует в')
+
     def __str__(self):
         return self.title
 
