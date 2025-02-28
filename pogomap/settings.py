734b9d4bf392c6b9a3e2ea39eee4ce79b7158d3e
gardnervile <lordy1jo@gmail.com>
2025-02-28 22:09:56 +0500

    Update settings.py

--- pogomap/settings.py
+++ pogomap/settings.py
@@ -16,7 +16,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'static'
 
 
 # Quick-start development settings - unsuitable for production
